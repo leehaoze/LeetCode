419c366f4722006375d4e6cecde3062e53ba96f5
leehaoze <986989635@qq.com>
2019-08-06 15:14:08 +0800

    两数相加

--- src/两数相加/Main.java
+++ src/两数相加/Main.java
@@ -4,12 +4,14 @@ package 两数相加;
 
 public class Main {
     public static void main(String args[]){
-        ListNode a1 = new ListNode(1);
+        ListNode a1 = new ListNode(7);
         ListNode a2 = new ListNode(8);
+        ListNode a3 = new ListNode(9);
         a1.next = a2;
-        ListNode b1 = new ListNode(0);
-        ListNode b2 = new ListNode(8);
-        // b1.next = b2;
+        a2.next = a3;
+        ListNode b1 = new ListNode(3);
+        ListNode b2 = new ListNode(2);
+        b1.next = b2;
 
 
         Solution solution = new Solution();
--- src/两数相加/Solution.java
+++ src/两数相加/Solution.java
@@ -21,6 +21,32 @@ class ListNode {
 
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0), head = result;
+        int carry = 0;
+        //这里可以一个 或 条件 只要一个不为null即可， 为null的那个赋个0就可以了
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            //计算进位
+            carry = sum / 10;
+            sum %= 10;
+            //赋值节点
+            result.next = new ListNode(sum);
+            result = result.next;
+            //移动指针节点
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        //如果仍然有进位
+        if (carry == 1) {
+            result.next = new ListNode(1);
+        }
+        return head.next;
+    }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode result = null, head = null;
         while (l1 != null && l2 != null) {
@@ -91,16 +117,4 @@ public class Solution {
 
         return head;
     }
-<<<<<<< HEAD
-=======
-
-    private int getLengthOfList(ListNode head) {
-        int length = 0;
-        while (head != null) {
-            length++;
-            head = head.next;
-        }
-        return length;
-    }
->>>>>>> 605f1bfeac621b5ced241fca22b2f84eda0133e6
 }
