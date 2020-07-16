a3f4436f8ebf82eca899661f8c15f17c4908eece
leehaoze <986989635@qq.com>
2020-07-16 19:17:01 +0800

    不同的二叉搜索树

--- src/_96/不同的二叉搜索树/Solution.java
+++ src/_96/不同的二叉搜索树/Solution.java
@@ -1,28 +1,41 @@
 package _96.不同的二叉搜索树;
 
 public class Solution {
-    public int cal(int n, int[] dict) {
-        if (n == 1) {
-            return 1;
-        }
-
-        if (dict[n] != 0) {
-            return dict[n];
-        }
-
-        int result = 0;
-        for (int i = 1; i < n - 1; i++) {
-            result += cal(i, dict) * cal(n - 1 - i, dict);
-        }
-
-        result += 2 * cal(n - 1, dict);
-
-        dict[n] = result;
-        return result;
-    }
-
+    //    public int cal(int n, int[] dict) {
+//        if (n == 1) {
+//            return 1;
+//        }
+//
+//        if (dict[n] != 0) {
+//            return dict[n];
+//        }
+//
+//        int result = 0;
+//        for (int i = 1; i < n - 1; i++) {
+//            result += cal(i, dict) * cal(n - 1 - i, dict);
+//        }
+//
+//        result += 2 * cal(n - 1, dict);
+//
+//        dict[n] = result;
+//        return result;
+//    }
+//
+//    public int numTrees(int n) {
+//        int dict[] = new int[n + 1];
+//        return cal(n, dict);
+//    }
     public int numTrees(int n) {
-        int dict[] = new int[n + 1];
-        return cal(n, dict);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[i - 1 - j] * dp[j];
+            }
+        }
+
+        return dp[n];
     }
 }
