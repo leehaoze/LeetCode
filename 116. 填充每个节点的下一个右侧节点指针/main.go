3e03420bb28d43f518bdf6c584ed1bba52e8cc4f
leehaoze <986989635@qq.com>
2021-03-22 23:23:58 +0800

    连接二叉树

---
+++ 116. 填充每个节点的下一个右侧节点指针/main.go
@@ -0,0 +1,29 @@
+package main
+
+*
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+
+	connectTwoNode(root.Left, root.Right)
+	return root
+}
+
+func connectTwoNode(nodeA *Node, nodeB *Node) {
+	if nodeA == nil || nodeB == nil {
+		return
+	}
+
+	nodeA.Next = nodeB
+	connectTwoNode(nodeA.Left, nodeA.Right)
+	connectTwoNode(nodeB.Left, nodeB.Right)
+	connectTwoNode(nodeA.Right, nodeB.Left)
+}
